5d215bbe60d5ede1908746f5991afad91c275b2f
Rhys Walker <rhys.h.walker@gmail.com>
2025-05-21 18:09:57 +0100

    Added primitive method for Array

--- src/vmobjects/VMVector.cpp
+++ src/vmobjects/VMVector.cpp
@@ -12,4 +12,31 @@
 #include "../vmobjects/ObjectFormats.h"
 #include "../vmobjects/VMObject.h"
 
-const size_t VMVector::VMVectorNumberOfFields = 0;
+const size_t VMVector::VMVectorNumberOfFields = 3;
+
+VMArray* VMVector::GetFieldArray(vm_oop_t obj) const {
+    auto* self = static_cast<VMVector*>(obj);
+    vm_oop_t rawStorage = self->GetField(2);
+    VMArray* storage = static_cast<VMArray*>(rawStorage);
+    return storage;
+}
+
+VMInteger* VMVector::GetFieldFirst(vm_oop_t obj) const {
+    auto* self = static_cast<VMVector*>(obj);
+    vm_oop_t rawStorage = self->GetField(0);
+    VMInteger* storage = static_cast<VMInteger*>(rawStorage);
+    return storage;
+}   
+
+VMInteger* VMVector::GetFieldLast(vm_oop_t obj) const {
+    auto* self = static_cast<VMVector*>(obj);
+    vm_oop_t rawStorage = self->GetField(1);
+    VMInteger* storage = static_cast<VMInteger*>(rawStorage);
+    return storage;
+}
+
+void VMVector::IndexOutOfBounds(size_t idx) const {
+    ErrorExit(("vector index out of bounds: Accessing " + to_string(idx) +
+               ", but vector size is only " + "\n")
+                  .c_str());
+}
