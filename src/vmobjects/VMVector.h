3f0ac9cc437d60328aa3c772624e2d3ed0aac2d2
Rhys Walker <rhys.h.walker@gmail.com>
2025-05-22 10:42:04 +0100

    fixed clang style options to pass CI

--- src/vmobjects/VMVector.h
+++ src/vmobjects/VMVector.h
@@ -16,8 +16,8 @@ public:
         int64_t const first = INT_VAL(load_ptr(this->first));
         int64_t const last = INT_VAL(load_ptr(this->last));
         VMArray* const storage = load_ptr(this->storage);
-        if (index < first || index > last) {
-            IndexOutOfBounds(index, (last-first));
+        if (index < 1 || index > first + last) {
+            IndexOutOfBounds(index, (last - first));
             // TODO(smarr): check if this would be correct
         }
         vm_oop_t returned = storage->GetIndexableField(first + index - 2);
