624a311787ee2fa84d0067321e9ce193eb7af4c7
Rhys Walker <rhys.h.walker@gmail.com>
2025-05-22 10:19:17 +0100

    Fixed error with IsValidObject failing on a Valid Object

--- src/vmobjects/VMVector.h
+++ src/vmobjects/VMVector.h
@@ -17,15 +17,15 @@ public:
         int64_t const last = INT_VAL(load_ptr(this->last));
         VMArray* const storage = load_ptr(this->storage);
         if (index < first || index > last) {
-            IndexOutOfBounds(index);
+            IndexOutOfBounds(index, (last-first));
             // TODO(smarr): check if this would be correct
         }
-        vm_oop_t returned = storage->GetIndexableField(first + index - 1);
+        vm_oop_t returned = storage->GetIndexableField(first + index - 2);
         return returned;
     }
 
     static __attribute__((noreturn)) __attribute__((noinline)) void
-    IndexOutOfBounds(size_t idx);
+    IndexOutOfBounds(size_t idx, size_t size);
 
 private:
     static const size_t VMVectorNumberOfFields;
